18eae105ae33128177343139e32cbd418cebdc61
tolozayurany <yalbarracint@gmail.com>
2021-06-24 16:25:09 -0500

    Se crea la primera función que hace el cambio de página

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import validator from './validator.js';
 
 console.log(validator);
 
+function showPage2() {
+    document.getElementById("pag1").style.display = "none";
+    document.getElementById("pag2").removeAttribute("hidden");
+    document.getElementById("pag2").style.display = "block";
+}
+
