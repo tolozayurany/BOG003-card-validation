2bb9a175bc62c42b151f0023b504ccb6842d10ac
tolozayurany <yalbarracint@gmail.com>
2021-06-28 14:43:56 -0500

    Se crea la función de añadir input en javascript para ingresar datos y almacenarlos

--- src/index.js
+++ src/index.js
@@ -2,14 +2,32 @@ import validator from './validator.js';
 
 console.log(validator);
 
-function showPage2() {
+//Funciones para manipular el DOM en el cambio de botones - Botón de continuar 
+document.addEventListener("DOMContentLoaded", function () {
+    var boton1 = document.getElementById('buttonContinue');
+            boton1.onclick = function(){
     document.getElementById("pag1").style.display = "none";
     document.getElementById("pag2").removeAttribute("hidden");
     document.getElementById("pag2").style.display = "block";
-}
+    }
+}, false);
 
-
-function showPage1() {
+//Funciones para manipular el DOM en el cambio de botones - Botón de regreso a la primera página
+document.addEventListener("DOMContentLoaded", function() {
+    var boton2 = document.getElementById('buttonReturn');
+            boton2.onclick = function(){
     document.getElementById("pag2").style.display = "none";
     document.getElementById("pag1").style.display = "block";
+    }
+}, false);
+
+//Funcion para crear input en la primera página, de tipo numérico para ingresar solo números
+//Revisar si necesito esta función porque no se, creo que ya tengo el imput en html 
+
+function createInput (){
+    var padre = document.getElementById("card");
+    var input = document.createElement("INPUT");
+    padre.appendChild(input);
 }
+
+
