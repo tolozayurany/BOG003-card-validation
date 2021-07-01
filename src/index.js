c8543a9293614e8cb9907f4c24b6e888a1173def
tolozayurany <yalbarracint@gmail.com>
2021-07-01 17:46:27 -0500

    modificaciones en el algoritmo Luhn, se validan las tarjetas pero todas se ven como -validas-, falta este arreglo

--- src/index.js
+++ src/index.js
@@ -1,35 +1,51 @@
-import validator from './validator.js';    
-
-console.log(validator);
+import validator from './validator.js';
 
 //Funciones para manipular el DOM en el cambio de botones - Botón de continuar 
+
 document.addEventListener("DOMContentLoaded", function () {
-    var boton1 = document.getElementById('buttonContinue');
-            boton1.onclick = function(){
-    document.getElementById("pag1").style.display = "none";
-    document.getElementById("pag2").removeAttribute("hidden");
-    document.getElementById("pag2").style.display = "block";
+    let button1 = document.getElementById('buttonContinue');
+    button1.onclick = function () {
+        document.getElementById("pag1").style.display = "none";
+        document.getElementById("pag2").removeAttribute("hidden");
+        document.getElementById("pag2").style.display = "block";
     }
 }, false);
 
 //Funciones para manipular el DOM en el cambio de botones - Botón de regreso a la primera página
-document.addEventListener("DOMContentLoaded", function() {
-    var boton2 = document.getElementById('buttonReturn');
-            boton2.onclick = function(){
-    document.getElementById("pag2").style.display = "none";
-    document.getElementById("pag1").style.display = "block";
+document.addEventListener("DOMContentLoaded", function () {
+    let button2 = document.getElementById('buttonReturn');
+    button2.onclick = function () {
+        document.getElementById("pag2").style.display = "none";
+        document.getElementById("pag1").style.display = "block";
     }
 }, false);
-
-
 
 //Funcion para crear input en la primera página, de tipo numérico para ingresar solo números
 //Revisar si necesito esta función porque no se, creo que ya tengo el imput en html 
 
-function createInput (){
+function createInput() {
     var padre = document.getElementById("card");
     var input = document.createElement("INPUT");
     padre.appendChild(input);
+    (createInput)
+};
+
+//función para llamar la variable de los números creada en el imput
+let creditCardNumber = document.getElementById("numbers").value;
+creditCardNumber;
+
+//Función para mostrar si la tarjeta es válida o no
+//Primero llamamos el evento del botón para que cuando se oprima el botón se ejecute la función
+ 
+document.addEventListener("click", function () {
+    let button3 = document.getElementById('buttonValidate'); //Nos llama el botón 
+    button3.onclick = function () {
+
+        let validation = validator.isValid(creditCardNumber);
+        if (validation == true) {
+            return document.getElementById('adIsValid').innerHTML = '<section id ="isValid" >Número de tarjeta correcto</section>';
+        }
+        else return document.getElementById('adIsValid').innerHTML = '<section id ="notValid">Número de tarjeta incorrecto</section>';
+    }
 }
-
-
+    , false);
--- src/validator.js
+++ src/validator.js
@@ -1,39 +1,27 @@
 const validator = {
-  // ...
+
+  isValid: function (creditCardNumber) {
+    //Empezamos llamando el valor la variable escrita en el imput con Id numbers
+
+    let numberSum = 0;
+    let validate = false;
+    //Se crea el for empezando desde el último número y continuando hacia atrás con el siguiente número hasta llegar a posición 0
+    for (let i = creditCardNumber.lenght - 1; i >= 0; i--) {
+            let stringDigit = creditCardNumber.charAt(i);  //Extrae el caracter en la posición n del string
+            let numberDigit = parseInt(stringDigit, 10); //Se asigna el valor del número en base 10 para convertir string en número entero
+
+      //Si la posición de este valor es par, entonces se multiplica el valor por 2
+
+        if (validate && (numberDigit *= 2) > 9) {
+          numberDigit -= 9; //Así se halla la suma de los dos digitos de cualquier numero mayor que 9
+        }
+      
+      numberSum += numberDigit; //Sumar todos los valores de la tarjeta
+      validate = !validate;
+    }
+    return (numberSum % 10) == 0; //Finaliza y da el valor boolean del residuo de la suma divivido en 10 igual a 0
+
+  }
+
 };
-
 export default validator;
-
-//Con documentContentLoaded le damos la función al botón de ejecutar la función de checkLuhn
-document.addEventListener("DOMContentLoaded", function() {
-  var boton2 = document.getElementById('buttonValidate');
-          boton2.onclick = function checkLuhn(value) {
- 
-//Empezamos llamando el valor la variable escrita en el imput con Id numbers
-  let creditCardNumber = document.getElementById("numbers").value;
-  let sum = 0;
-  
-//Se crea el for empezando desde el último número y continuando hacia atrás con el siguiente número hasta llegar a posición 0
-  for(var i = value.lenght-1; i>=0; n--){
-      var digit = value.charAt(n);  
-      var  nDigit = paraseInt(digit,10); //Se asigna el valor del número en base 10 para .....????
-
-//Si la posición de este valor es par, entonces se multiplica el valor por 2
-      if (i % 2 == 0){
-        nDigit *= 2 ;
-        if (nDigit > 9){
-          nDigit -= 9; //Así se halla la suma de los dos digitos de cualquier numero mayor que 9
-
-          sum += nDigit; //Sumar todos los valores de la tarjeta
-        }
-
-          return (sum % 10) == 0; //Finaliza y da el valor del residuo de la suma divivido en 10 igual a 0
-
-      }    
-
-        }
-
-
-
-   }
-} ,false );
